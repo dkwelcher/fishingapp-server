ab5a2dc0c291347b8f32a798c81a23cd93024e9a
dkwelcher <dkwelcher@gmail.com>
2023-10-31 12:45:10 -0400

    Initial commit

---
+++ src/main/java/com/fishinglog/fishingapp/FishingAppApplication.java
@@ -0,0 +1,13 @@
+package com.fishinglog.fishingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fishinglog/fishingapp/FishingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishinglog.fishingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

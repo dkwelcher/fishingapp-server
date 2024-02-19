a6d80a40f5c4aa98b9eecdb817511ee8666bb791
dkwelcher <dkwelcher@gmail.com>
2024-02-19 10:11:15 -0500

    Added method findByUsername

--- src/main/java/com/fishinglog/fishingapp/repositories/UserRepository.java
+++ src/main/java/com/fishinglog/fishingapp/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.fishinglog.fishingapp.domain.entities.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
 
 }

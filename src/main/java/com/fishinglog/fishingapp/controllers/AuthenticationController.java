07c5e74f57e49c92512810e899558bd587410aa3
dkwelcher <dkwelcher@gmail.com>
2024-02-19 10:15:43 -0500

    Added controller for authentication & register requests

---
+++ src/main/java/com/fishinglog/fishingapp/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.fishinglog.fishingapp.controllers;
+
+import com.fishinglog.fishingapp.domain.auth.AuthenticationRequest;
+import com.fishinglog.fishingapp.domain.auth.AuthenticationResponse;
+import com.fishinglog.fishingapp.domain.auth.RegisterRequest;
+import com.fishinglog.fishingapp.services.auth.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}

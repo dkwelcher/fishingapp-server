f19f2542d2f12356b2256756b34efe27b2f71d8d
dkwelcher <dkwelcher@gmail.com>
2024-03-14 18:13:49 -0400

    Changed token expiration to 24 hours
    Expiration was previously mistakenly set to 24 minutes

--- src/main/java/com/fishinglog/fishingapp/services/auth/JwtService.java
+++ src/main/java/com/fishinglog/fishingapp/services/auth/JwtService.java
@@ -33,14 +33,13 @@ public class JwtService {
 
     public String generateToken(
             Map<String, Object> extractClaims,
-            UserDetails userDetails
-    ) {
+            UserDetails userDetails) {
         return Jwts
                 .builder()
                 .setClaims(extractClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 hours
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }

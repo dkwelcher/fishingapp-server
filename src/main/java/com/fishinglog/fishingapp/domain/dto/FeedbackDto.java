2f4fef33ec5646a6af6e1c50460c5fd3bfada23c
dkwelcher <dkwelcher@gmail.com>
2024-03-19 17:07:13 -0400

    Added feedback object for collecting user feedback data

---
+++ src/main/java/com/fishinglog/fishingapp/domain/dto/FeedbackDto.java
@@ -0,0 +1,15 @@
+package com.fishinglog.fishingapp.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FeedbackDto {
+
+    private String feedback;
+}

239fe4606abac3dbd622af4b3f85cf385b12b33d
dkwelcher <dkwelcher@gmail.com>
2024-03-19 17:06:20 -0400

    Added ownership check

--- src/main/java/com/fishinglog/fishingapp/controllers/WeatherController.java
+++ src/main/java/com/fishinglog/fishingapp/controllers/WeatherController.java
@@ -2,6 +2,8 @@ package com.fishinglog.fishingapp.controllers;
 
 import com.fishinglog.fishingapp.domain.weather.WeatherDto;
 import com.fishinglog.fishingapp.services.WeatherService;
+import com.fishinglog.fishingapp.services.auth.OwnershipService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +17,24 @@ public class WeatherController {
 
     private final WeatherService weatherService;
 
-    public WeatherController(WeatherService weatherService) {
+    private final OwnershipService ownershipService;
+
+    public WeatherController(WeatherService weatherService, OwnershipService ownershipService) {
         this.weatherService = weatherService;
+        this.ownershipService = ownershipService;
     }
 
-    // GET /weather?latitude=11.11&longitude=11.11
+    // GET /weather?userId=123&latitude=11.11&longitude=11.11
     @GetMapping(path = "/weather")
     public ResponseEntity<WeatherDto> getCurrentWeather(
+            @RequestParam(value = "userId") Long userId,
             @RequestParam(value = "latitude") double latitude,
-            @RequestParam(value = "longitude") double longitude) {
+            @RequestParam(value = "longitude") double longitude,
+            HttpServletRequest request) {
+
+        if(!ownershipService.doesRequestUsernameMatchTokenUsername(userId, request)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
 
         WeatherDto weatherDto = weatherService.getCurrentWeather(latitude, longitude);
         return new ResponseEntity<>(weatherDto, HttpStatus.OK);

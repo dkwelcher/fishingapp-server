fd8cc6f139b7a4c8b93d47660c5cf0529bf01865
dkwelcher <dkwelcher@gmail.com>
2024-10-15 13:55:14 -0400

    Added jakarta constraint annotations to attributes
    Constraint annotations are used in conjunction with @Valid before @RequestBody in controller methods to enforce validation.

--- src/main/java/com/fishinglog/fishingapp/domain/dto/CatchDto.java
+++ src/main/java/com/fishinglog/fishingapp/domain/dto/CatchDto.java
@@ -1,5 +1,6 @@
 package com.fishinglog.fishingapp.domain.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +11,7 @@ import java.time.LocalTime;
 /**
  * Data Transfer Object for a fishing catch, encapsulating all relevant details.
  *
- * @since 2023-10-31
+ * @since 2024-10-15
  */
 @Data
 @AllArgsConstructor
@@ -20,23 +21,52 @@ public class CatchDto {
 
     private Long catchId;
 
+    @NotNull
     private LocalTime time;
 
+    @NotNull
+    @DecimalMin(value = "-90.0")
+    @DecimalMax(value = "90.0")
     private Double latitude;
 
+    @NotNull
+    @DecimalMin(value = "-180.0")
+    @DecimalMax(value = "180.0")
     private Double longitude;
 
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z ]*$")
+    @Size(max = 50)
     private String species;
 
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z ]*$")
+    @Size(max = 50)
     private String lureOrBait;
 
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z ]*$")
+    @Size(max = 25)
     private String weatherCondition;
 
+    @NotNull
+    @Min(value = -50)
+    @Max(value = 150)
     private Integer airTemperature;
 
+    @NotNull
+    @Min(value = -50)
+    @Max(value = 150)
     private Integer waterTemperature;
 
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 150)
     private Integer windSpeed;
 
+    @NotNull
     private TripDto trip;
 }

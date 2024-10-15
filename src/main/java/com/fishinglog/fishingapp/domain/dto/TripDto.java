65997a8c0a93adcc542f181924a57bbb6589db11
dkwelcher <dkwelcher@gmail.com>
2024-10-15 14:38:02 -0400

    Added jakarta validation constraint annotations
    Jakarta validation constrain annotations are used in conjunction with @Valid annotation in TripController class. Defined validation methods in TripController class are removed.

--- src/main/java/com/fishinglog/fishingapp/domain/dto/TripDto.java
+++ src/main/java/com/fishinglog/fishingapp/domain/dto/TripDto.java
@@ -1,5 +1,9 @@
 package com.fishinglog.fishingapp.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,9 +24,15 @@ public class TripDto {
 
     private Long tripId;
 
+    @NotNull
     private LocalDate date;
 
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9 ]*$")
+    @Size(max = 100)
     private String bodyOfWater;
 
+    @NotNull
     private UserDto user;
 }
